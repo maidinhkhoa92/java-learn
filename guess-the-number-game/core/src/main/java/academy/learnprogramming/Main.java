557fb2369ba10eb9a8bd0127519088090856b27e
maidinhkhoa92 <maidinhkhoa92@gmail.com>
2020-06-18 22:01:31 +0700

    update

---
+++ guess-the-number-game/core/src/main/java/academy/learnprogramming/Main.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private final static Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Hello world");
+        log.debug("Hlelo debug");
+    }
+}
