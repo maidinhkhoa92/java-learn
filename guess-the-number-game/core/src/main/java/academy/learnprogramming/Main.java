cd3b8a61c2196a510c0d85310c930587e3b1e97e
maidinhkhoa92 <maidinhkhoa92@gmail.com>
2020-06-24 22:18:22 +0700

    update

--- guess-the-number-game/core/src/main/java/academy/learnprogramming/Main.java
+++ guess-the-number-game/core/src/main/java/academy/learnprogramming/Main.java
@@ -2,12 +2,29 @@ package academy.learnprogramming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     private final static Logger log = LoggerFactory.getLogger(Main.class);
 
+    private static final String CONFIG_LOCATION = "beans.xml";
+
     public static void main(String[] args) {
         log.info("Hello world");
-        log.debug("Hlelo debug");
+
+        // create context (container)
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+
+        // call method next() to generator number
+        int number = numberGenerator.next();
+
+        // log generator number
+        log.info("number = {}", number);
+
+        // close context (container)
+        context.close();
     }
 }
